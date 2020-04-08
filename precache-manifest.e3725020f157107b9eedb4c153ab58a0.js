6977875412dd185edda6a86a4bbb4df8da54d1e7
Shashi <shashi_athadu1@yahoo.com>
2020-04-08 08:56:18 +0530

    Updates

---
+++ precache-manifest.e3725020f157107b9eedb4c153ab58a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcaa47f6c322a727a44a35882bd3651",
+    "url": "/COVID-QUIZ/index.html"
+  },
+  {
+    "revision": "94d90caf89b89e6f6345",
+    "url": "/COVID-QUIZ/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cba8ef54330f9bbe9abf",
+    "url": "/COVID-QUIZ/static/css/main.5010274b.chunk.css"
+  },
+  {
+    "revision": "94d90caf89b89e6f6345",
+    "url": "/COVID-QUIZ/static/js/2.ef38d414.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/COVID-QUIZ/static/js/2.ef38d414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba8ef54330f9bbe9abf",
+    "url": "/COVID-QUIZ/static/js/main.29c5c8cb.chunk.js"
+  },
+  {
+    "revision": "19c1eb23199fcab4c7af",
+    "url": "/COVID-QUIZ/static/js/runtime-main.75d52ab0.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/COVID-QUIZ/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/COVID-QUIZ/static/media/logo.5d5d9eef.svg"
+  }
+]);
